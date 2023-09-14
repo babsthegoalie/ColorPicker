de9964cbfb2155b3f1ec47268e28af94d9dbeddb
babsthegoalie <bzkondracki@gmail.com>
2023-09-13 21:39:45 -0600

    modified:   package-lock.json 	modified:   src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
 // Write your Color component here
 
+const Color = (props) => {
+  return <div className="red"></div>
+}
+
+
+
 const App = () => {
   return (
     <div id="container">
@@ -7,7 +13,11 @@ const App = () => {
         <div>Currently selected: </div>
         <div className="red">red</div>
       </div>
-      <div id="colors-list">{/* colors go here */}</div>
+      <div id="colors-list">
+  <Color />
+  <Color />
+  <Color />
+</div>
     </div>
   );
 };
